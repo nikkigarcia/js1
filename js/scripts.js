3204c310c633945c0a9a51b59bdeb81b8316e621
Nikki Garcia <yurfavorite_girl@yhaoo.com>
2018-05-23 16:51:31 -0700

    takehome

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 var myName = "Nikki";
 //comments
 console.log(myName);
+
+var hobby = "From this class, I would like to learn how to add a product to a cart for purchasing purposes.My favorite hobby is indoor soccer!";
+//comments
+console.log(hobby);
