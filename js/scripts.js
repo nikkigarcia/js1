b2893bdb2b835b000b391a1875a06cc8653be274
Nikki Garcia <yurfavorite_girl@yhaoo.com>
2018-05-23 21:02:30 -0700

    day2

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var myName = "Nikki";
+//comments
+console.log(myName);
+
+var hobby = "From this class, I would like to learn how to add a product to a cart for purchasing purposes.My favorite hobby is indoor soccer!";
+//day2
+console.log(hobby);
+
+var name = "Nikki Garcia";
+let birthday = new Date('7.2.1988');
+const goal = "To become a Web Developer";
+console.log(name, birthday, goal);
+//console.log(typeof obj) primative string or object
+let favFood = "Chicken Fettuccine Alfredo";
+let favMovie = 'Bruce Almighty';
+//array V
+const ingredients = ["salt", "olive oil", "chicken breast", "pepper", "butter", "heavy cream", "nutmeg", "parmigiano-reggiano cheese"];
+const movieYear = 2003;
+alert(favFood + ingredients);
+confirm(favMovie + movieYear);
