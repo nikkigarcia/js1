c7acfae0548135518e21ce8544a1a3c90061cfd5
Nikki Garcia <yurfavorite_girl@yhaoo.com>
2018-06-03 20:57:35 -0700

    favoriteMovies

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+let favMovie = ['Bruce Almighty', 'Bruce'];
+let secondFav = ['The Proposal', 'Andrew Paxton'];
+let thirdFav = ['21 Jumpstreet', 'Schmidt' ];
+let fourthFav = ['The Note Book', 'Noah'];
+let fithFav = ['How to Loose a Guy in 10 Days', 'Andie Anderson'];
+
+let favoriteMovies = ['Bruce Almighty', 'The Proposal', '21 Jumpstreet', 'The Note Book','How to Loose a Guy in 10 Days' ];
+alert(favoriteMovies);
+
+let firstLast = ['Bruce Almighty', 'How to Loose a Guy in 10 Days'];
+console.log(firstLast);
+alert(favoriteMovies.length);
