635c23911d1193f88acb52e3ec486edcc19fd8e4
Nikki Garcia <yurfavorite_girl@yhaoo.com>
2018-11-14 17:43:22 -0800

    day3notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,12 @@
-var myName = "Nikki";
-//comments
-console.log(myName);
-
-var hobby = "From this class, I would like to learn how to add a product to a cart for purchasing purposes.My favorite hobby is indoor soccer!";
-//day2
-console.log(hobby);
-
-var name = "Nikki Garcia";
-let birthday = new Date('7.2.1988');
-const goal = "To become a Web Developer";
-console.log(name, birthday, goal);
-//console.log(typeof obj) primative string or object
-let favFood = "Chicken Fettuccine Alfredo";
-let favMovie = 'Bruce Almighty';
-//array V
-const ingredients = ["salt", "olive oil", "chicken breast", "pepper", "butter", "heavy cream", "nutmeg", "parmigiano-reggiano cheese"];
-const movieYear = 2003;
-alert(favFood + ingredients);
-confirm(favMovie + movieYear);
-
-
-let favColor = Prompt('give me your fav color', '...')
-switch(favColor.upperLowerCase()) {//normalize the data
-
-}
+//
+// let favColor = Prompt('give me your fav color', '...')
+// switch(favColor.upperLowerCase()) {//normalize the data
+//
+// }
+// loose equal ==
+// strict equal ===
+// loose not equal !=
+// strict not equal !==
+// console.log('42'== 42);
+// true
+// objects are never equal even if they have the same values
