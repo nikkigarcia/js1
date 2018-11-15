fb368f37279d207f8608677eba1632c4951271cc
Nikki Garcia <yurfavorite_girl@yhaoo.com>
2018-11-14 20:42:59 -0800

    day4done

--- js/scripts.js
+++ js/scripts.js
@@ -79,7 +79,7 @@ today.shift();// removes first membeer
 
 today.splice(2, 1, false, "new");// remove starting at index 2 and everything after
 console.log(today, "this is the result of a splice")
-// today.splice(2, 1); second parameter is the amout you want to delete after the splice
+// today.splice(2, 1); second parameter is the amount you want to delete after the splice
 
 const products = [['blanket',12.99]]
 let copy = products.slice(); //to make a copy for frame works like react
@@ -89,12 +89,74 @@ console.log(today.indexOf(2006));//use indexOf/lastIndexOf to find out the index
 //alert(products[0][1]); =12.99
 
 let favoriteMovie = ['Bruce Almighty', 'Bruce'];
-let secondFav = ['The Proposal'];
-let thirdFav = ['21 Jumpstreet'];
+let secondFav = ['The Proposal', 'Ben'];
+let thirdFav = ['21 Jumpstreet', 'Jeff'];
+let fourthFav = ['The Note Book', 'Noah']
 
-let favoriteMovies = [];
+let favoriteMovies = [favMovie, secondFav, thirdFav, fourthFav];
 
-favoriteMovies.push(favMovie, secondFav, thirdFav);
-
+favoriteMovies.push(favMovie, secondFav, thirdFav, fourthFav);
+alert(favoriteMovies);
 let titles = [];
 titles.push(favoriteMovies[0] [0], favoriteMovies [1] [0]);
+alert(titles);
+console.log('least fav', thirdFav);
+alert(favoriteMovies.length);
+
+const names = ['Nikki', 'Keda', 'Jesus', 'Naomi', 'Adrian', 'Baby'];
+
+const parents = names.slice(0,2);
+const nums = [1,2,3,4,5,'nikki',6,7,8,9,20];
+nums.indexOf('nikki');
+// will return -1 if not a part of index
+
+const luckyNums = [2,4,12,15,21,31,6,25,2];
+luckyNums.indexOf(2);
+// will return the first instance of a duplicate number or word
+
+const str = 'racecar';
+str === str.split('').reverse().join('')
+names.sort();
+// alphabetical order
+
+// // loops
+// no point in using while or do while cuz its old school
+for(let i = 0; i < = 10,; i++){
+  console.log('Nikki');
+}
+const names = ['Nicole','Islatia', 'Jewel', 'Leigh'];
+
+for(let i = 0; i < names.length; i++){
+ console.log(names[i]);
+}
+// this list out names
+
+for(let name of names){
+  console.log(name);
+}
+// this is easier to list out names
+// one down side is you can't leave out first or last names...
+names.forEach(name => console.log(name));
+// I want to take this array and look at each number
+names.forMap(name => console.log(name));
+// objects uses curly braces V
+const person = {
+  name: 'Nikki',
+  age: 30
+};
+console.log(person);
+
+for(let key in Person){
+  console.log(person[key]);
+}
+console.log(person.name);
+const user = {};
+user.name = prompt('What is your name?');
+user.age = prompt('How old are you?');
+consol.log(user);
+// returns both nikki and 29 if you only want name consol.log(user.name);
+
+for(let favoriteMovie of favoriteMovies){
+  console.log(favoriteMovie);
+}
+alert(favoriteMovie.year);
