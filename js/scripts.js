3df4155f3d65bd67fb50ca4934a947dd052ebc47
Nikki Garcia <yurfavorite_girl@yhaoo.com>
2018-11-21 18:51:59 -0800

    day5thdone

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function numberSquared{
+  let answer = prompt('Type a number');
+  console.log(answer)
+
+}
+numberSquared(answer * answer);
+let answer = (prompt('Type a number'));
+
+function capitalize{
+  str  = ('');
+
+}
